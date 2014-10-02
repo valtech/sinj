d21d91d978341208f5cff9ef8b106c34351c55bc
Richard Tappenden <richard.tappenden@trueclarity.co.uk>
2014-10-02 15:01:44 +0100

    Add files

---
+++ Scripts/sc-media.js
@@ -0,0 +1,7 @@
+﻿var scInsertMediaItem = function (packet) {
+    scInsertItem(packet);
+
+    // Set the media
+    var item = scItem(packet.id,null,null);
+    $scMediaItem.attachMedia(item, packet.fileName, packet.media64);
+}
